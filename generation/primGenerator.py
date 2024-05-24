a58b99de4f93b0b3c5e8b266ec5558991dd951a9
s3952320 <s3952320@student.rmit.edu.au>
2024-05-24 20:55:43 +1000

    prim complete + cfg files

--- generation/primGenerator.py
+++ generation/primGenerator.py
@@ -1,27 +1,72 @@
 # -------------------------------------------------------------------
-# DON'T CHANGE THIS FILE.
+# PLEASE UPDATE THIS FILE.
 # Prim's maze generator.
 #
 # __author__ = 'Jeffrey Chan'
 # __copyright__ = 'Copyright 2024, RMIT University'
 # -------------------------------------------------------------------
 
-
+from random import randint, choice
 from maze.maze3D import Maze3D
 from maze.util import Coordinates3D
 from generation.mazeGenerator import MazeGenerator
 
 
-
 class PrimMazeGenerator(MazeGenerator):
     """
-    Prim's algorithm maze generator.  
+    Prim's algorithm maze generator.
     TODO: Complete the implementation (Task A)
     """
-	
 
-    def generateMaze(self, maze:Maze3D):
-        # TODO: Implement this method for task A.
-        pass
+    def generateMaze(self, maze: Maze3D):
+        # Initialize the maze with all walls
+        maze.initCells(True)
 
-        
+        # Select a random starting cell
+        startLevel = randint(0, maze.levelNum() - 1)
+        startCoord: Coordinates3D = Coordinates3D(startLevel, randint(0, maze.rowNum(startLevel) - 1),
+                                                  randint(0, maze.colNum(startLevel) - 1))
+
+        # Create a set to store visited cells
+        visited: set[Coordinates3D] = set([startCoord])
+
+        # Create a list to store the frontier cells
+        frontier: list[Coordinates3D] = []
+
+        # Add the neighbors of the starting cell to the frontier
+        for neigh in maze.neighbours(startCoord):
+            if self.isValidCell(maze, neigh):
+                frontier.append(neigh)
+
+        while frontier:
+            # Randomly select a cell from the frontier
+            currCell: Coordinates3D = choice(frontier)
+
+            # Find the visited neighbors of the current cell
+            visitedNeighs: list[Coordinates3D] = [neigh for neigh in maze.neighbours(currCell) if neigh in visited]
+
+            # If there are visited neighbors
+            if visitedNeighs:
+                # Randomly select a visited neighbor
+                neigh = choice(visitedNeighs)
+
+                # Remove the wall between the current cell and the selected neighbor
+                maze.removeWall(currCell, neigh)
+
+                # Add the current cell to the visited set
+                visited.add(currCell)
+
+            # Remove the current cell from the frontier
+            frontier.remove(currCell)
+
+            # Add the unvisited neighbors of the current cell to the frontier
+            for neigh in maze.neighbours(currCell):
+                if neigh not in visited and neigh not in frontier and self.isValidCell(maze, neigh):
+                    frontier.append(neigh)
+
+        # Update maze generated flag
+        self.m_mazeGenerated = True
+
+    def isValidCell(self, maze: Maze3D, cell: Coordinates3D) -> bool:
+        level, row, col = cell.getLevel(), cell.getRow(), cell.getCol()
+        return 0 <= level < maze.levelNum() and 0 <= row < maze.rowNum(level) and 0 <= col < maze.colNum(level)
